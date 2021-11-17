1197d2294380e4f6f839f6c50ff290e1cab661a2
Sneha Kothi <sneha.kothi@somaiya.edu>
2021-11-17 23:55:02 +0530

    Fruits and veggies added

--- vegbox/vegbox_app/urls.py
+++ vegbox/vegbox_app/urls.py
@@ -3,6 +3,7 @@ from vegbox_app import views
 from users import views as user_views
 urlpatterns = [
     path('', views.home,name='vegbox-home'),
+    path('fruits/', views.fruits,name='vegbox-fruits'),
     path('register/', user_views.register, name='register'),
     path('register_details/', user_views.register_details, name='vegbox-details'),
 
--- vegbox/vegbox_app/views.py
+++ vegbox/vegbox_app/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse, response
 # Create your views here.
 def home(request):
     return render(request,'vegbox_app/home.html')
+def fruits(request):
+    return render(request,'vegbox_app/fruits.html')
